d33f351609261dc262a3bd1c50cdebae48c60bf3
Kölcze Szabolcs <kolcze.szabolc2007@gmail.com>
2026-01-30 09:04:37 +0100

    Suspect osztály létrehozva

---
+++ Digitalis nyomozas/Suspect.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digitalis_nyomozas
+{
+	internal class Suspect
+	{
+		private Person suspectData;
+		private int wantedLevel;
+
+		public Suspect(Person suspectData, int wantedLevel)
+		{
+			this.suspectData = suspectData;
+			this.wantedLevel = wantedLevel;
+		}
+
+		public int WantedLevel { get => wantedLevel; set => wantedLevel = value; }
+		internal Person SuspectData { get => suspectData; set => suspectData = value; }
+	}
+}
