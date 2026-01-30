c05a510de04252b2f81da0c1900d3285a7f92623
Kölcze Szabolcs <kolcze.szabolc2007@gmail.com>
2026-01-30 09:00:44 +0100

    Person osztály létrehozva

---
+++ Digitalis nyomozas/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digitalis_nyomozas
+{
+	internal class Person
+	{
+		private string nev;
+		private int eletKor;
+		private string megjegyzes;
+
+		public Person(string nev, int eletKor, string megjegyzes)
+		{
+			this.nev = nev;
+			this.eletKor = eletKor;
+			this.megjegyzes = megjegyzes;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public int EletKor { get => eletKor; set => eletKor = value; }
+		public string Megjegyzes { get => megjegyzes; set => megjegyzes = value; }
+	}
+}
