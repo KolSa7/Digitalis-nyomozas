b3b0e0e217869bda402114d9a1bc08651bdabda5
Kölcze Szabolcs <kolcze.szabolc2007@gmail.com>
2026-01-30 09:18:07 +0100

    TimelineEvent osztály létrehozva

---
+++ Digitalis nyomozas/TimelineEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digitalis_nyomozas
+{
+	internal class TimelineEvent
+	{
+		private string datum;
+		private string leiras;
+		public TimelineEvent(string datum, string leiras)
+		{
+			this.datum = datum;
+			this.leiras = leiras;
+		}
+		public string Datum { get => datum; set => datum = value; }
+		public string Leiras { get => leiras; set => leiras = value; }
+	}
+}
