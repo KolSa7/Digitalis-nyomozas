7c71b819dc54fd30288dbfa695c6797238f957d5
Kölcze Szabolcs <kolcze.szabolc2007@gmail.com>
2026-01-30 08:56:49 +0100

    CaseStatus osztály létrehozva

---
+++ Digitalis nyomozas/CaseStatus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digitalis_nyomozas
+{
+	internal class CaseStatus
+	{
+		private string status;
+
+		public CaseStatus(string status)
+		{
+			this.status = status;
+		}
+
+		public string Status { get => status; set => status = value; }
+	}
+}
