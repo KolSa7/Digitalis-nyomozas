5d7564cd0c4786d167aa4cad53a00c157a52c47f
Diák <diak@petrik.hu>
2026-02-06 10:23:01 +0100

    Interface kiépítése elkezdve

---
+++ Digitalis nyomozas/Fo.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digitalis_nyomozas
+{
+	internal class Fo
+	{
+		private CentralDatabase adatbazis;
+		public Fo(CentralDatabase adatbazis, DecisionEngine dontes)
+		{
+			this.Adatbazis = adatbazis;
+		}
+		internal CentralDatabase Adatbazis { get => adatbazis; set => adatbazis = value; }
+
+		public void createCase()
+		{
+			CaseManager ugykezelo = new CaseManager(adatbazis);
+			Console.WriteLine("Adja meg az ügy címét:");
+			string cim = Console.ReadLine();
+			Console.WriteLine("Adja meg az ügy leírását:");
+			string leiras = Console.ReadLine();
+			Console.Clear();
+			ugykezelo.UjUgy(cim, leiras);
+			Console.ReadKey();
+			string valasz;
+			do {
+				Console.WriteLine("Szeretne bizonyítékot hozzáadni? (igen/nem)");
+				valasz = Console.ReadLine();
+				if (valasz.ToLower() == "igen")
+				{
+					Console.WriteLine("Adja meg a bizonyíték típusát:");
+					string tipus = Console.ReadLine();
+					Console.WriteLine("Adja meg a bizonyíték leírását:");
+					string bizonyitekLeiras = Console.ReadLine();
+					Console.WriteLine("Adja meg a bizonyíték megbízhatóságát (1-10):");
+					int megbizhatosag = int.Parse(Console.ReadLine());
+					Console.Clear();
+					ugykezelo.UjBizonyitek(tipus, bizonyitekLeiras, megbizhatosag, adatbazis.Ugyek.Last());
+					Console.ReadKey();
+					Console.Clear();
+				}
+			}
+			while (valasz.ToLower()=="igen");
+
+			do
+			{
+				Console.WriteLine("Szeretne személyeket hozzáadni?(igen/nem)");
+				valasz = Console.ReadLine();
+				Console.Clear();
+				if (valasz.ToLower() == "igen")
+				{
+					Console.WriteLine("Adja meg a személy nevét:");
+					string nev = Console.ReadLine();
+					Console.WriteLine("Adja meg a személy életkorát:");
+					int eletkor = int.Parse(Console.ReadLine());
+					Console.WriteLine("Adja meg a személy nemét:");
+					string nem = Console.ReadLine();
+					Person szemely = new Person(nev, eletkor, nem);
+					Console.Clear();
+					ugykezelo.UjSzemely(szemely, adatbazis.Ugyek.Last());
+					Console.ReadKey();
+					Console.Clear();
+				}
+			}
+			while (valasz.ToLower() == "igen");
+		}
+
+	}
+}
--- Digitalis nyomozas/Program.cs
+++ Digitalis nyomozas/Program.cs
@@ -2,17 +2,18 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+		static void Main(string[] args)
         {
-            CentralDatabase kozpont=new CentralDatabase();
+            /*CentralDatabase kozpont=new CentralDatabase();
             CaseManager ugykezelo=new CaseManager(kozpont);
             ugykezelo.UjUgy("Eltűnt személy", "Egy 30 éves férfi eltűnt a városban.");
             ugykezelo.UgyLista();
             ugykezelo.UjBizonyitek("Fénykép", "Egy fénykép a helyszínről.", 8, kozpont.Ugyek[0]);
             Person szemely=new Person("John Doe", 30, "Férfi");
-            ugykezelo.UjSzemely(szemely, kozpont.Ugyek[0]);
+            ugykezelo.UjSzemely(szemely, kozpont.Ugyek[0]);*/
 
 
 		}
+        
     }
 }
